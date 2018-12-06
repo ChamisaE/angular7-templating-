3df10472a9cbd07e673d0ef441297a0045074cb5
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-06 10:43:18 -0700

    full form from before deletes

--- src/app/post/posts.component.ts
+++ src/app/post/posts.component.ts
@@ -8,28 +8,28 @@ import {Status} from "../shared/interfaces/status";
 	template: require("./posts.component.html")
 })
 
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit{
 	posts: Post[];
-	postForm: FormGroup;
-	status: Status = {status:null, message: null, type: null};
+	postForm : FormGroup;
+	status : Status = {status:null, message:null, type: null};
 
-	constructor(private postService: PostService, private formBuilder: FormBuilder) {
-	}
+	constructor(private postService: PostService, private formBuilder : FormBuilder) {}
 
 	ngOnInit() {
 		this.postService.getAllPosts().subscribe(posts => this.posts = posts);
+
 		this.postForm = this.formBuilder.group({
-			postTitle: ["", [Validators.maxLength(64), Validators.required]],
-			postContent: ["", [Validators.maxLength(1024), Validators.required]]
+			postTitle : ["", [Validators.maxLength(64), Validators.required]],
+			postContent : ["", [Validators.maxLength(1024), Validators.required]]
 		});
-		this.loadPosts()
+		this.loadPosts();
 	}
 
 	loadPosts() : void {
 		this.postService.getAllPosts().subscribe(posts => this.posts = posts);
 	}
-	createPost(): void {
-		let post :Post = {postId : null, postContent: this.postForm.value.postContent, postDate: null, postTitle: this.postForm.value.postTitle};
+	createPost() : void {
+		let post :Post = {postId: null, postContent: this.postForm.value.postContent,postDate: null, postTitle: this.postForm.value.postTitle};
 
 		this.postService.createPost(post).subscribe(status => {
 			this.status = status;
@@ -39,5 +39,4 @@ export class PostsComponent implements OnInit {
 			}
 		})
 	}
-
 }
